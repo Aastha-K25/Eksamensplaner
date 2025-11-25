a270e275c735ed8aba2008c136e0762c3ee38661
Eylem003 <eyy001@edu.zealand.dk>
2025-11-25 22:05:54 +0100

    Skema - nye sider

--- Eksamensplaner.Web/Pages/SkemaStuderende.cshtml.cs
+++ Eksamensplaner.Web/Pages/SkemaStuderende.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Eksamensplaner.Pages;
 
-public class StuderneLogInd : PageModel
+public class SkemaStuderende : PageModel
 {
     public void OnGet()
     {
